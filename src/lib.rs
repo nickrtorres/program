522c353476d096a2c7e7ca27ddb84731c8dd9283
Nick Torres <nickrtorres@icloud.com>
2020-03-27 20:48:49 -0700

    program: initial commit
    Signed-off-by: Nick Torres <nickrtorres@icloud.com>

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! Program provides a single divergent method `perror` to emulate C's `perror`
+
+/// Store and expose a static, shared reference to a process name
+pub struct Program {
+    pub name: &'static str,
+}
+
+impl Program {
+    pub fn new(name: &'static str) -> Self {
+        Program { name }
+    }
+
+    /// Print an error message to `stderr` and exit with an exit status of 1.
+    pub fn perror<E: std::fmt::Display>(&self, e: E) -> ! {
+        eprintln!("{}: {}", self.name, e);
+        std::process::exit(1)
+    }
+}
