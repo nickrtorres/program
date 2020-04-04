da2bee686f5ea4ef30cb1031ff7bc24b4460b694
Nick Torres <nickrtorres@icloud.com>
2020-04-03 20:03:17 -0700

    program: provide `perror` as a free function
    The previous implementation required a user to hold onto an instance of
    a `program::Program`. This was not ideal since this crate's C analogue
    exists as a free function. Remove `program::Program` in favor of a free
    `perror` function.
    
    Signed-off-by: Nick Torres <nickrtorres@icloud.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,33 @@
 //! Program provides a single divergent method `perror` to emulate C's `perror`
 
-/// Store and expose a static, shared reference to a process name.
-pub struct Program {
-    pub name: &'static str,
+#![warn(clippy::pedantic)]
+
+use std::ffi;
+use std::io::{self, Write};
+use std::process;
+
+/// Print an error message to `stderr` and exit with an exit status of `1`.
+///
+/// While this function attempts to obtain and write the  name of the current process to `stderr`,
+/// write `e` to `stderr`, and exit with an exit status of `1`, only exiting is guranteed.
+/// Unfortunately a lot can go wrong when getting the name of the current process.
+/// If  for any reason the name of the current process cannot be formed, just print
+/// the error given by the user. If the error given by the use cannot be written to stderr just
+/// exit with an exit status of 1.
+pub fn perror<E: std::fmt::Display>(e: E) -> ! {
+    if let Some(cmd) = process_name() {
+        let _ = write!(io::stderr(), "{}: ", cmd.to_string_lossy());
+    }
+
+    let _ = writeln!(io::stderr(), "{}", e);
+    process::exit(1)
 }
 
-impl Program {
-    /// Create a new `Program` that holds a static, shared reference to a program name.
-    pub fn new(name: &'static str) -> Self {
-        Program { name }
-    }
-
-    /// Print an error message to `stderr` and exit with an exit status of 1.
-    pub fn perror<E: std::fmt::Display>(&self, e: E) -> ! {
-        eprintln!("{}: {}", self.name, e);
-        std::process::exit(1)
-    }
+/// Attempt to obtain the name of the current process.
+///
+/// Return `Some(process_name)` or `None` if it cannot be obtained.
+fn process_name() -> Option<ffi::OsString> {
+    let cmd = std::env::current_exe().map_or(None, Some)?;
+    let process = cmd.file_name()?;
+    Some(process.to_os_string())
 }
